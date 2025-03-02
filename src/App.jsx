79ad00b8da8c9206751cea022e24e58490d456c4
yoonii324 <jyoonii324@gmail.com>
2025-03-01 23:43:21 -0500

    src: main.jsx App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import Home from './pages/Home'
+import AddBook from './pages/AddBook'
+import EditBook from './pages/EditBook'
+import BookDetails from './pages/BookDetails'
+import Error from './components/Error'
+
 const App = () => {
   return (
-    <></>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add" element={<AddBook />} />
+        <Route path="/edit/:id" element={<EditBook />} />
+        <Route path="/books/:id" element={<BookDetails />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Layout>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,13 @@
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { BookProvider } from './context/BookContext'
+import App from './App'
+import './index.css'
+
 createRoot(document.getElementById('root')).render(
-  <></>
+  <BrowserRouter>
+    <BookProvider>
+      <App />
+    </BookProvider>
+  </BrowserRouter>
 )
